4b9521e1c825cc7367db447391a7175b0c6a909a
Hailey <42811174+haileyme@users.noreply.github.com>
2018-10-25 22:30:25 +0900

    Update 181-employees-earning-more-than-their-managers.sql

--- 181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers.sql
@@ -1,1 +1,1 @@
-select a.Name "Employee" from Employee a, Employee b where a.ManagerID = b.Id and a.Salary > b.Salary
+select a.Name "Employee" from Employee a, Employee b where a.ManagerID = b.Id and a.Salary > b.Salary;
